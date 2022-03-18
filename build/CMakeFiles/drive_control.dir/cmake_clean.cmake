48f03eb8d2c41053e80675b3baaecfc3d1f9b7a6
likecanyon <1174578375@qq.com>
2022-03-18 22:39:42 +0800

    drive

---
+++ build/CMakeFiles/drive_control.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drive_control.dir/drive_control.cpp.o"
+  "drive_control"
+  "drive_control.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drive_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
