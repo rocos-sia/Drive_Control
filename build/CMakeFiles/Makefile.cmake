f2d36ce37f931b8aa66288518cf1ad8332dbe465
likecanyon <1174578375@qq.com>
2022-03-19 11:59:47 +0800

    修复

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -46,5 +46,4 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/drive_control.dir/DependInfo.cmake"
-  "CMakeFiles/main.dir/DependInfo.cmake"
   )
