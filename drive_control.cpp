0398a6819314ff80ff6127c9ad271c7e7f5854ef
likecanyon <1174578375@qq.com>
2022-03-28 21:47:39 +0800

    使用checkSum后

--- drive_control.cpp
+++ drive_control.cpp
@@ -101,7 +101,7 @@ void checkSum(unsigned char *sendBuffer)
         sum = sum + sendBuffer[i];
     }
     sendBuffer[8] = sum;
-
+    sum = 0;
 }
 
 int main(int argc, char **argv)
@@ -116,12 +116,14 @@ int main(int argc, char **argv)
 
     unsigned char sendBuffer[] = {0x55, 0XAA, 0x04, 0x02, 0x03, 0x37, 0x00, 0x00, 0X58};
     checkSum(sendBuffer);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     serialPort.write_some(boost::asio::buffer(sendBuffer, 9));
 
     sendBuffer[6] = {0x14};
     sendBuffer[7] = {0x05};
     checkSum(sendBuffer);
-    serialPort.write_some(boost::asio::buffer(sendBuffer,9));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    serialPort.write_some(boost::asio::buffer(sendBuffer, 9));
 
     return 0;
 }
